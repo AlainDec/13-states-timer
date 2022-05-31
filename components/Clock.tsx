9e257497eef045cda1be561e449206c703ab1c2b
unknown <jtwest@free.fr>
2022-05-31 12:23:09 +0200

    add typescript

--- components/Clock.tsx
+++ components/Clock.tsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text } from 'react-native';
 
-const Clock = () => {
+const Clock: React.FC = () => {
 
-    const [dateTime, setDateTime] = useState(new Date());
+    const [dateTime, setDateTime] = useState<Date>(new Date());
 
     useEffect( () => {
-        const id = setInterval( () => setDateTime(new Date()), 1000 );
+        const id: NodeJS.Timer  = setInterval( () => setDateTime(new Date()), 1000 );
 
         return () => {
             clearInterval(id);
